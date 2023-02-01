7ec63460a1e2f46d8402e3bd07d3751d80dcefa2
VieTGioIHOa <vietntph22574@fpt.edu.vn>
2023-02-01 12:19:51 +0700

    History purchase feature

--- src/pages/User/pages/HistoryPurchase/HistoryPurchase.tsx
+++ src/pages/User/pages/HistoryPurchase/HistoryPurchase.tsx
@@ -1,5 +1,84 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { createSearchParams, Link } from 'react-router-dom'
+import purchaseApi from 'src/api/purchase.api'
+import path from 'src/constants/path'
+import { purchasesStatus } from 'src/constants/purchase'
+import useQueryParams from 'src/hooks/useQueryParams'
+import { formatCurrency, generateNameId } from 'src/utils/utils'
+import { PurchaseListStatus } from 'src/types/purchase.type'
 
+const purchaseTabs = [
+  { status: purchasesStatus.all, name: 'Tất cả' },
+  { status: purchasesStatus.waitForConfirmation, name: 'Chờ xác nhận' },
+  { status: purchasesStatus.waitForGetting, name: 'Chờ lấy hàng' },
+  { status: purchasesStatus.inProgress, name: 'Đang giao' },
+  { status: purchasesStatus.delivered, name: 'Đã giao' },
+  { status: purchasesStatus.cancelled, name: 'Đã hủy' }
+]
 export default function HistoryPurchase() {
-  return <div>HistoryPurchase</div>
+  const queryParams: { status?: string } = useQueryParams()
+  const status: number = Number(queryParams.status) || purchasesStatus.all
+
+  const { data: purchasesHistory } = useQuery({
+    queryKey: ['purchases', { status }],
+    queryFn: () => purchaseApi.getPurchases({ status: status as PurchaseListStatus })
+  })
+
+  const purchasesInCart = purchasesHistory?.data.data
+  return (
+    <div>
+      <div className='overflow-x-auto'>
+        <div className='min-w-[700px]'>
+          <div className='sticky top-0 flex rounded-t-sm shadow-sm'>
+            {purchaseTabs.map((tab) => (
+              <Link
+                to={{
+                  pathname: path.historyPurChase,
+                  search: createSearchParams({ status: String(tab.status) }).toString()
+                }}
+                key={tab.status}
+                className={`${
+                  status === tab.status ? 'border-b-orange text-orange' : 'border-b-black/10 text-gray-900'
+                } flex flex-1 items-center justify-center border-b-2  bg-white py-4 text-center `}
+              >
+                {tab.name}
+              </Link>
+            ))}
+          </div>
+          <div>
+            {purchasesInCart?.map((purchase) => (
+              <div key={purchase._id} className='mt-4 rounded-sm border-black/10 bg-white p-6 text-gray-800 shadow-sm'>
+                <Link
+                  to={`${path.home}${generateNameId({ name: purchase.product.name, id: purchase.product._id })}`}
+                  className='flex'
+                >
+                  <div className='flex-shrink-0'>
+                    <img className='h-20 w-20 object-cover' src={purchase.product.image} alt={purchase.product.name} />
+                  </div>
+                  <div className='ml-3 flex-grow overflow-hidden'>
+                    <div className='truncate'>{purchase.product.name}</div>
+                    <div className='mt-3'>x{purchase.buy_count}</div>
+                  </div>
+                  <div className='ml-3 flex-shrink-0'>
+                    <span className='truncate text-gray-500 line-through'>
+                      ₫{formatCurrency(purchase.product.price_before_discount)}
+                    </span>
+                    <span className='ml-2 truncate text-orange'>₫{formatCurrency(purchase.product.price)}</span>
+                  </div>
+                </Link>
+                <div className='flex justify-end'>
+                  <div>
+                    <span>Tổng giá tiền</span>
+                    <span className='ml-4 text-xl text-orange'>
+                      ₫{formatCurrency(purchase.product.price * purchase.buy_count)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
